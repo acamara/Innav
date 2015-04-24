1d6267f7d3fa351d71fccf0de1e465a8afef40c4
Albert Cámara <albertcamara@hotmail.es>
2015-04-24 12:48:27 +0200

    Update function calculate_Accuracy_custom

--- ScriptsR/calculate_Accuracy_custom.R
+++ ScriptsR/calculate_Accuracy_custom.R
@@ -1,7 +1,7 @@
-calculate_Accuracy_custom <- function(rssi, txPower){
+calculate_Accuracy_custom <- function(rssi, RSSi_0){
   # Calculate accuracy with custom path loss model
   
-  xg = 0; n = 6; d0=1;
-  accuracy = d0*(10^((-rssi+txPower-xg)/(10*n)))
+  n = 5; xg = 0; d0=1;
+  accuracy = d0*(10^((-rssi+RSSi_0-xg)/(10*n)))
   accuracy
 }
