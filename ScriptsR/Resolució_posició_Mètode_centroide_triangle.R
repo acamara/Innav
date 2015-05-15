ccf22a5bc76a4f2f926bddcff7889dcebf3d653d
Albert Cámara <albertcamara@hotmail.es>
2015-05-15 21:13:18 +0200

    Update calculate_Position_Centroid Script

--- ScriptsR/Resolució_posició_Mètode_centroide_triangle.R
+++ ScriptsR/Resolució_posició_Mètode_centroide_triangle.R
@@ -1,15 +1,13 @@
 # Resolució de la posició mitjançant Centroid Positioning
-
 # Clean out the workspace and console
 rm(list=ls())      
 cat("\014")
 
-# Definim les coordenades dels beacons
-b1x = 0; b1y = 2.2;
-b2x = 3; b2y = 4.4;
-b3x =  6; b3y = 0;
+# Definim les coordenades dels quatre punts definits per la intersecció de quatre arcs
+b1x = 1.66; b1y = 3.13;
+b2x = 1.14; b2y = 2.78;
+b3x = 2.52; b3y = 1.63;
+b4x = 3.30; b4y = 2.08;
 
-# Càlcul de la posició (xu,yu) mitjançant el càlcul del centroide que defineixen tres Beacons 
-sol = c((b1x+b2x+b3x)/3,(b1y+b2y+b3y)/3)
-
-sol
+# Càlcul de la posició (xu,yu) mitjançant el càlcul del centroide del polígon 
+sol = c((b1x+b2x+b3x+b4x)/4,(b1y+b2y+b3y+b4y)/4)
