1afdeb0242a95e3ad900a6903172734aec7b4f48
Albert Cámara <albertcamara@hotmail.es>
2015-05-19 10:04:28 +0200

    Update script measures at one meter

--- ScriptsR/Beacon_All_Mean_RSSI_1_meter_multiple_iterations.R
+++ ScriptsR/Beacon_All_Mean_RSSI_1_meter_multiple_iterations.R
@@ -22,10 +22,22 @@ d2 <- calculate_mean_rssi_all_beacons_1_meter(path)
 path <- "./DATASET_BEACONS/measures_at_one_meter/d3/"
 d3 <- calculate_mean_rssi_all_beacons_1_meter(path)
 
+path <- "./DATASET_BEACONS/measures_at_one_meter/d4/"
+d4 <- calculate_mean_rssi_all_beacons_1_meter(path)
+
+path <- "./DATASET_BEACONS/measures_at_one_meter/d5/"
+d5 <- calculate_mean_rssi_all_beacons_1_meter(path)
+
+path <- "./DATASET_BEACONS/measures_at_one_meter/d6/"
+d6 <- calculate_mean_rssi_all_beacons_1_meter(path)
+
 data <- data.frame("D0:F8:F2:95:3A:5C" = numeric(0),"D6:40:86:6D:BD:DF" = numeric(0),"D9:A3:28:6A:A7:F1" = numeric(0),"F5:7A:A5:E3:EF:18" = numeric(0),"FF:A6:7C:AF:B0:CB" = numeric(0), stringsAsFactors=FALSE)
 
 data[nrow(data) + 1,] <- d1
 data[nrow(data) + 1,] <- d2
 data[nrow(data) + 1,] <- d3
+data[nrow(data) + 1,] <- d4
+data[nrow(data) + 1,] <- d5
+data[nrow(data) + 1,] <- d6
 
 summary(data)
